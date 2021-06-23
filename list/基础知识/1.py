5a3e69850178ed2e6581adb8bc64bb7fe2765771
koray <347466123@qq.com>
2021-06-23 17:02:10 +0800

    add

--- list/基础知识/1.py
+++ list/基础知识/1.py
@@ -7,13 +7,12 @@ import requests
 import sys
 from math import sin as s
 
-r = requests.get('https://www.baidu.com/')
-print r.status_code
+# r = requests.get('https://www.baidu.com/')
+# print r.status_code
 # print r.text
 # print r.encoding
 # print r.url
 
-print s(3)
 
 # =======================  数据类型  =====================================
 
@@ -26,8 +25,75 @@ print s(3)
 a = 2
 b = 232
 e = complex(float(a), float(b))
-print id(a)  # 获得对象的id标识
-print ['a:', type(a), 'b:', type(b), 'e:', type(e)]  # 获取当前数据的数据类型
-print e  # (2+232j)
-print e.real  # 2.0
-print e.imag  # 232.0
+# print id(a)  # 获得对象的id标识
+# print ['a:', type(a), 'b:', type(b), 'e:', type(e)]  # 获取当前数据的数据类型
+# print e  # (2+232j)
+# print e.real  # 2.0
+# print e.imag  # 232.0
+
+
+# --------------  string Type ----------------
+
+# 定义字符串可以使用单引号,双引号,三引号等三种方式;
+#
+#
+# 字符串输出形式
+# 1. %s(字符串),%d(整型)
+# 2. {0} + format()
+# 3. +形式, 前后都只能是string
+# 4. ,形式
+
+
+_str = '123456789'
+
+# 字符串截取:变量[开始:结束:步长], 从第1个开始到第8个且每隔1个字符
+print(_str[0:8:2])
+
+# 字符串内容为浮点型要转换为整型时,要先转化成float再转换成int
+print(int(float("2.1")))
+
+
+
+
+
+
+#
+# # 文件目录操作
+# import os
+# print os.getcwd()  #获取当前的文件目录结构
+# os.chdir("xx/xx")  #修改当前的文件目录结构,相当于复制文件
+#
+#
+#
+#
+#
+#
+#
+# # 如何使用python来产生随机数
+# # 为什么要有随机数？
+# # 1）利用正态分布产生一些列的随机数，模拟现实生活中一些场景
+# # 2）二项分布、beta分布
+#
+# #产品0-1之间的随机数
+# import rendom
+# print random.random() 取[0,1)内的随机小数
+# print random.randint(1,10) 取[0,10]内的随机小数
+# print random.randrange(1,10,2)
+#
+#
+# # numpy包
+# import numpy as np
+# np.random.rendom(size(3,2)) #产生0-1之间的符合均匀分布的随机数
+# #产生0-1之间的8个随机数
+# np.random.randint(1,10,8)
+# #产生正态分布的随机数
+# pn.random.randn()
+# pn.random.randn(2,4)  #参数表示矩阵size为2行4列
+# #产生二项分布的随机数
+# np.random.binomial(10,0.5,(2,3)) #第一个参数表示均值,二表示方差,三表示矩阵的size
+# # 产生卡方分布的随机数
+# np.rendom.chisquare(2,(2,3)) #第一个参数表示自由度
+# #gama伽马分布
+# np.random.gamma(2,4,100) #前两个参数表示自由度
+#
+#
--- list/基础知识/diff_py2_py3.py
+++ list/基础知识/diff_py2_py3.py
@@ -3,10 +3,16 @@
 # @Author : koray
 # @File : py2与py3的不同点
 
+# 1.默认的编码不同
+# py2默认编码为ascii,py3默认编码为utf-8
+# 故py2使用中文时,需要在py文件第一行注释   # -*- coding: utf-8 -
 
-# 1. input输入语句不同
+# 2.print打印语句不同
+print "hello!"   # py2独有,可不用括号
+print("hello!")  # py2与py3都能用
+
+# 3.input键输入语句不同
 # py3中从input输入的均为string类型,如果需要整形或浮点型需要强转;
 # raw_input:获取到的输入永远都是str类型,py3就没有这个方法,已整合到input中
 # input:获取到的输入会自动判断其类型,字符格式必须加上单引号或者双引号
 print(int(input("请输入整数..")), type(input("随便输入..")))
-
